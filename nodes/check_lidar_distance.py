923d7302766347f85e5ad27867ee33cee3bc6dd5
Aryan Indarapu <aryan1pro@gmail.com>
2024-06-06 17:07:05 -0500

    added lidar distance node, needs to be tested

---
+++ nodes/check_lidar_distance.py
@@ -0,0 +1,43 @@
+import rospy
+from sensor_msgs.msg import LaserScan
+
+from pyjoycon import JoyCon, get_R_id, get_L_id
+
+class CheckLidarDistance:
+    def __init__(self):
+        self.lidar_sub = rospy.Subscriber('/scan', LaserScan, self.lidar_callback)
+        self.l_joycon = JoyCon(get_L_id())
+        self.l_joycon.set_rumble(0, 0)
+        self.l_joycon.set_leds([0, 0, 0, 0])
+        self.l_joycon.set_player_lights(1)
+        self.l_joycon.set_home_light(1)
+        self.l_joycon.set_rumble(0, 0)
+        
+        self.r_joycon = JoyCon(get_R_id())
+        self.r_joycon.set_rumble(0, 0)
+        self.r_joycon.set_leds([0, 0, 0, 0])
+        self.r_joycon.set_player_lights(1)
+        self.r_joycon.set_home_light(1)
+        self.r_joycon.set_rumble(0, 0)
+
+    def lidar_callback(self, msg):
+        # TODO: check intensities and ranges (discard values not between range_min and range_max)
+        min_distance = min(msg.ranges)
+        if min_distance < 0.5:
+            if min_distance < 0.2:
+                self.l_joycon.set_rumble(0.5, 0.5)
+                self.r_joycon.set_rumble(0.5, 0.5)
+            else:
+                if msg.ranges.index(min_distance) < len(msg.ranges) / 2:
+                    self.l_joycon.set_rumble(0.5, 0)
+                else:
+                    self.r_joycon.set_rumble(0.5, 0)
+        else:
+            self.l_joycon.set_rumble(0, 0)
+            self.r_joycon.set_rumble(0, 0)
+
+
+if __name__ == '__main__':
+    rospy.init_node('check_lidar_distance')
+    cld = CheckLidarDistance()
+    rospy.spin()
